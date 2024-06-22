788fd02faf71516d5a566a18bcae3749a9d9fd99
Jack Michaud <jackmichaud04@icloud.com>
2024-06-22 19:59:13 -0400

    pdf viewing errors

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 import base64
+
 import streamlit as st
-from streamlit_pdf_viewer import pdf_viewer
 
 
-uploaded_files = st.file_uploader(
-    "Choose a PDF file",
-    accept_multiple_files=True,
-    type=['pdf']
-)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
+uploaded = st.file_uploader(label="Please browse for a pdf file", type="pdf")
+method = st.radio(label="Method", options=["Embed", "Iframe"], horizontal=True)
+if uploaded is None:
+    st.stop()
 
-    st.write("filename:", uploaded_file.name)
-    base64_pdf = base64.b64encode(bytes_data).decode('utf-8')
+base64_pdf = base64.b64encode(uploaded.read()).decode("utf-8")
+if method == "Embed":
+    pdf_display = (
+        f'<embed src="data:application/pdf;base64,{base64_pdf}" '
+        'width="800" height="1000" type="application/pdf"></embed>'
+    )
+elif method == "Iframe":
+    pdf_display = (
+        f'<iframe src="data:application/pdf;base64,{base64_pdf}" '
+        'width="800" height="1000" type="application/pdf"></iframe>'
+    )
+else:
+    st.Error(f"Unknown method: {method}")
     
-    pdf_viewer(input=uploaded_file)
-
-    
-    # pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    # st.markdown(pdf_display, unsafe_allow_html=True)
+st.markdown(pdf_display, unsafe_allow_html=True)
